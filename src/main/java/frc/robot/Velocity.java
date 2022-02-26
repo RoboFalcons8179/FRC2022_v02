c613d50a847fac118fd4b0e0b516b639f99ae148
RoboFalcons8179 <dcrobotics@gmail.com>
2022-02-26 14:33:55 -0500

    actually adding the vel to the merging path

--- src/main/java/frc/robot/Velocity.java
+++ src/main/java/frc/robot/Velocity.java
@@ -1,65 +1,7 @@
-/**
- * Phoenix Software License Agreement
- *
- * Copyright (C) Cross The Road Electronics.  All rights
- * reserved.
- * 
- * Cross The Road Electronics (CTRE) licenses to you the right to 
- * use, publish, and distribute copies of CRF (Cross The Road) firmware files (*.crf) and 
- * Phoenix Software API Libraries ONLY when in use with CTR Electronics hardware products
- * as well as the FRC roboRIO when in use in FRC Competition.
- * 
- * THE SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT
- * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
- * LIMITATION, ANY WARRANTY OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT SHALL
- * CROSS THE ROAD ELECTRONICS BE LIABLE FOR ANY INCIDENTAL, SPECIAL, 
- * INDIRECT OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF
- * PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
- * BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE
- * THEREOF), ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER
- * SIMILAR COSTS, WHETHER ASSERTED ON THE BASIS OF CONTRACT, TORT
- * (INCLUDING NEGLIGENCE), BREACH OF WARRANTY, OR OTHERWISE
- */
-
-/**
- * Description:
- * The VelocityClosedLoop_AuxStraightIntegratedSensor example demonstrates the new Talon/Victor auxiliary
- * and remote features used to perform complex closed loops. This example has the robot performing 
- * Velocity closed loop with an auxiliary closed loop on integrated sensor difference (Heading)
- * to keep the robot straight.
- * 
- * This example uses:
- * - 2x Falcon 500 Integrated Sensors, one on both sides of robot for Primary Closed Loop on Velocity
- * A Talon/Victor calculates the distance by taking the sum of both sensors and dividing it by 2.
- * - 2x Falcon 500 Integrated Sensors, one of both side of robot for Auxiliary Closed Loop on Heading
- * A Talon/Victor calculates the heading by taking the difference between both sensors.
- * 
- * This example has two modes of operation, which can be switched between with Button 2.
- * 1.) Arcade Drive
- * 2.) Velocity Closed Loop with integrated sensor and drive straight with integrated
- * sensors difference
- * 
- * Controls:
- * Button 1: When pressed, zero all sensors. Set integrated sensor' positions to 0.
- * Button 2: When pressed, toggle between Arcade Drive and Velocity Closed Loop.
- * 	When toggling into Velocity Closed Loop, the current heading is saved and used
- * 	as the the auxiliary closed loop target. Can be changed by toggling out and in again.
- * Left Joystick Y-Axis: 
- * 	+ Arcade Drive: Drive robot forward and reverse
- * 	+ Velocity Closed Loop: Servo robot forward and reverse [-500, 500] RPM
- * Right Joystick X-Axis:
- * 	+ Arcade Drive: Turn robot left and right
- * 	+ Velocity Closed Loop: Not used
- * 
- * Gains for Velocity Closed Loop and Auxiliary may need to be adjusted in Constants.java
- * 
- * Supported Version:
- * - Talon FX: 20.2.3.0
- */
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Joystick;
 
 
@@ -71,87 +13,87 @@ import com.ctre.phoenix.motorcontrol.FollowerType;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+public class Velocity {
 
-public class Robot extends TimedRobot {
-	/** Hardware */
-	WPI_TalonFX _leftMaster = new WPI_TalonFX(5);
-	WPI_TalonFX _rightMaster = new WPI_TalonFX(9);
+    WPI_TalonSRX _leftMaster;
+    WPI_TalonSRX _rightMaster;
 
-	WPI_VictorSPX _leftFollow = new WPI_VictorSPX(2);
-	WPI_VictorSPX _rightFollow = new WPI_VictorSPX(6);
-	Joystick _gamepad = new Joystick(0);
-	
-	/** Latched values to detect on-press events for buttons */
-	boolean[] _btns = new boolean[Constants.kNumButtonsPlusOne];
-	boolean[] btns = new boolean[Constants.kNumButtonsPlusOne];
+    VictorSPX _leftFollow;
+    VictorSPX _rightFollow;
 
-	/** Invert Directions for Left and Right */
-	boolean _leftInvert = false; //Same as invert = "false"
-	boolean _rightInvert = true; //Same as invert = "true"
+    	/** Config Objects for motor controllers */
+	TalonSRXConfiguration _leftConfig = new TalonSRXConfiguration();
+	TalonSRXConfiguration _rightConfig = new TalonSRXConfiguration();
+    
+    
+    public boolean _leftInvert = false; 
+	public boolean _rightInvert = true;
 
-	/** Config Objects for motor controllers */
-	TalonFXConfiguration _leftConfig = new TalonFXConfiguration();
-	TalonFXConfiguration _rightConfig = new TalonFXConfiguration();
-	
-	/** Tracking variables */
-	boolean _firstCall = false;
-	boolean _state = false;
-	double _lockedDistance = 0;
-	double _targetAngle = 0;
+    // These will not change if the direction changes. This is for
+    public boolean _leftFollowSame = true;
+    public boolean _rightFollowSame = true;
+
+    public double maxSpeed = 500;
 
 
-	@Override
-	public void robotInit() {
-		/* Not used in this example */
-	}
+    public Velocity( 
+        WPI_TalonSRX _leftMasterIn, WPI_TalonSRX _rightMasterIn,
+        WPI_VictorSPX _leftFollowIn, WPI_VictorSPX _rightFollowIn) {
+        // left master, right master, left follow, right follow
+        
 
-	@Override
-	public void teleopInit(){
-		/* Disable all motor controllers */
-		_rightMaster.set(0);
-		_leftMaster.set(0);
+        // Bring in Motors
+        _leftMaster = _leftMasterIn;
+        _rightMaster = _rightMasterIn;
+
+        _leftFollow = _leftFollowIn;
+        _rightFollow = _rightFollowIn;
+
 		
 		/* Set Neutral Mode */
 		_leftMaster.setNeutralMode(NeutralMode.Brake);
 		_rightMaster.setNeutralMode(NeutralMode.Brake);
+        _leftFollow.setNeutralMode(NeutralMode.Brake);
+        _rightFollow.setNeutralMode(NeutralMode.Brake);
 
-		/* Configure output */
+		/* Configure output Inversions */
 		_leftMaster.setInverted(_leftInvert);
 		_rightMaster.setInverted(_rightInvert);
-		/*
-		 * Talon FX does not need sensor phase set for its integrated sensor
-		 * This is because it will always be correct if the selected feedback device is integrated sensor (default value)
-		 * and the user calls getSelectedSensor* to get the sensor's position/velocity.
-		 * 
-		 * https://phoenix-documentation.readthedocs.io/en/latest/ch14_MCSensor.html#sensor-phase
-		 */
-        // _leftMaster.setSensorPhase(true);
-        // _rightMaster.setSensorPhase(true);
-		
-		/** Closed loop configuration */
-		
-		/* Configure the left Talon's selected sensor as integrated sensor */
+
+
+        // Look up the Ternary operator. If the polarity of the follow motor is the 
+        // same as the drive motor, it will set it to the drive motor inversion. 
+        // Otherwise, it will be the opposite.
+        _leftFollow.setInverted(_leftFollowSame ? _leftInvert : !_leftInvert);
+        _rightFollow.setInverted(_rightFollowSame ? _rightInvert : !_rightInvert);
+
+
+        /* Configure the left Talon's selected sensor as integrated sensor */
 		/* 
 		 * Currently, in order to use a product-specific FeedbackDevice in configAll objects,
 		 * you have to call toFeedbackType. This is a workaround until a product-specific
 		 * FeedbackDevice is implemented for configSensorTerm
 		 */
-		_leftConfig.primaryPID.selectedFeedbackSensor = TalonFXFeedbackDevice.IntegratedSensor.toFeedbackDevice(); //Local Feedback Source
-
+        _leftConfig.primaryPID.selectedFeedbackSensor = TalonSRXFeedbackDevice.QuadEncoder.toFeedbackDevice();
+        //IntegratedSensor.toFeedbackDevice(); //Local Feedback Source
 		/* Configure the Remote (Left) Talon's selected sensor as a remote sensor for the right Talon */
 		_rightConfig.remoteFilter1.remoteSensorDeviceID = _leftMaster.getDeviceID(); //Device ID of Remote Source
 		_rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.TalonFX_SelectedSensor; //Remote Source Type
-		
-		/* Setup difference signal to be used for turn when performing Drive Straight with encoders */
+
 		setRobotTurnConfigs(_rightInvert, _rightConfig);
 
-		/* Config the neutral deadband. */
+        /* Config the neutral deadband. */
 		_leftConfig.neutralDeadband = Constants.kNeutralDeadband;
 		_rightConfig.neutralDeadband = Constants.kNeutralDeadband;
 
@@ -163,7 +105,7 @@ public class Robot extends TimedRobot {
 		_rightConfig.peakOutputForward = +1.0;
 		_rightConfig.peakOutputReverse = -1.0;
 
-		/* FPID Gains for velocity servo */
+        		/* FPID Gains for velocity servo */
 		/* FPID for Distance */
 		_rightConfig.slot2.kF = Constants.kGains_Velocit.kF;
 		_rightConfig.slot2.kP = Constants.kGains_Velocit.kP;
@@ -180,8 +122,9 @@ public class Robot extends TimedRobot {
 		_rightConfig.slot1.kD = Constants.kGains_Turning.kD;
 		_rightConfig.slot1.integralZone = Constants.kGains_Turning.kIzone;
 		_rightConfig.slot1.closedLoopPeakOutput = Constants.kGains_Turning.kPeakOutput;
-			
-		/* 1ms per loop.  PID loop can be slowed down if need be.
+
+
+        /* 1ms per loop.  PID loop can be slowed down if need be.
 		 * For example,
 		 * - if sensor updates are too slow
 		 * - sensor deltas are very small per update, so derivative error never gets large enough to be useful.
@@ -194,10 +137,9 @@ public class Robot extends TimedRobot {
 		_rightConfig.slot3.closedLoopPeriod = closedLoopTimeMs;
 		
 		/* APPLY the config settings */
-		//_leftMaster.configAllSettings(_leftConfig);
-		//_rightMaster.configAllSettings(_rightConfig);
+		_leftMaster.configAllSettings(_leftConfig);
+		_rightMaster.configAllSettings(_rightConfig);
 
-		//_leftFollow.configAllSettings(_leftConfig);
 		
 		/* Set status frame periods to ensure we don't have stale data */
 		_rightMaster.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 20, Constants.kTimeoutMs);
@@ -206,96 +148,53 @@ public class Robot extends TimedRobot {
 		_leftMaster.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 5, Constants.kTimeoutMs);
 
 		/* Initialize */
-		_firstCall = true;
-		_state = false;
 		zeroSensors();
-	}
-	
-	@Override
-	public void teleopPeriodic() {
-		/* Gamepad processing */
-		double forward = -1 * _gamepad.getY();
-		double turn = _gamepad.getRawAxis(4);
-		forward = Deadband(forward);
-		turn = Deadband(turn);
-	
-		/* Button processing for state toggle and sensor zeroing */
-		getButtons(btns, _gamepad);
-		if(btns[2] && !_btns[2]){
-			_state = !_state; 	// Toggle state
-			_firstCall = true;	// State change, do first call operation
-			_targetAngle = _rightMaster.getSelectedSensorPosition(1);
-		}else if (btns[1] && !_btns[1]) {
-			zeroSensors();		// Zero sensors
-		}
-		System.arraycopy(btns, 0, _btns, 0, Constants.kNumButtonsPlusOne);
-				
-		if(!_state){
-			if (_firstCall)
-				System.out.println("This is Arcade Drive.\n");
-			
-			_leftMaster.set(forward);
-			_rightMaster.set(forward);
 
-			/* Uncomment to find total encoer units per rotation */
-			// System.out.println("Diff: " + _rightMaster.getSelectedSensorPosition(1));
-		}else{
-			if (_firstCall) {
-				System.out.println("This is Velocity Closed Loop with the Auxiliary PID using integrated sensor.");
-				System.out.println("Travel [-500, 500] RPM in either direction while also maintaining a straight heading.\n");
-				
-				/* Determine which slot affects which PID */
-				_rightMaster.selectProfileSlot(Constants.kSlot_Velocit, Constants.PID_PRIMARY);
-				_rightMaster.selectProfileSlot(Constants.kSlot_Turning, Constants.PID_TURN);
-			}	
-			/* Calculate targets from gamepad inputs */
-			double target_RPM = forward * 10000;	// +- 500 RPM
-			double target_unitsPer100ms = target_RPM * Constants.kSensorUnitsPerRotation / 600.0;
-			double target_turn = _targetAngle;
-			
-			/* Configured for Velocity Closed Loop on Integrated Sensors' Sum and Auxiliary PID on Integrated Sensors' Difference */
-			_rightMaster.set(target_unitsPer100ms);
-			_leftMaster.follow(_rightMaster, FollowerType.AuxOutput1);
 
-			_rightFollow.follow(_rightMaster, FollowerType.PercentOutput);
-			_leftFollow.follow(_leftMaster, FollowerType.PercentOutput);
+    }
 
-      System.out.println(_rightMaster.getSelectedSensorVelocity());
-      System.out.println(_leftMaster.getSelectedSensorVelocity());
-		}
-		_firstCall = false;
-	}
-	
-	/** Zero integrated sensor on Talons */
-	void zeroSensors() {
-		_leftMaster.getSensorCollection().setIntegratedSensorPosition(0, Constants.kTimeoutMs);
-		_rightMaster.getSensorCollection().setIntegratedSensorPosition(0, Constants.kTimeoutMs);
+
+
+    public void vel_initalize() {
+
+        zeroSensors();
+
+    }
+
+    public void velPeriodic(double speed, double turn, boolean vel) {
+
+        // Re-initalize the loops
+        _rightMaster.selectProfileSlot(Constants.kSlot_Velocit, Constants.PID_PRIMARY);
+        _rightMaster.selectProfileSlot(Constants.kSlot_Turning, Constants.PID_TURN);
+
+        double target_RPM = speed;	// +- 1000 RPM
+        double target_unitsPer100ms = target_RPM * Constants.kSensorUnitsPerRotation / 600.0;
+        double target_turn = turn;
+
+        if (vel) {
+            _rightMaster.set(target_unitsPer100ms);
+            _leftMaster.follow(_rightMaster, FollowerType.AuxOutput1);
+
+            _rightFollow.follow(_rightMaster, FollowerType.PercentOutput);
+            _leftFollow.follow(_leftMaster, FollowerType.PercentOutput);
+        } else {
+            
+        }
+    }
+
+    public void zeroSensors () {
+        
+        _leftMaster.setSelectedSensorPosition(0);
+        _rightMaster.setSelectedSensorPosition(0);
+
 		System.out.println("[Integrated Sensors] All sensors are zeroed.\n");
-	}
-	
-	/** Deadband 5 percent, used on the gamepad */
-	double Deadband(double value) {
-		/* Upper deadband */
-		if (value >= +0.05) 
-			return value;
-		
-		/* Lower deadband */
-		if (value <= -0.05)
-			return value;
-		
-		/* Outside deadband */
-		return 0;
-	}
-	
-	/** Gets all buttons from gamepad */
-	void getButtons(boolean[] btns, Joystick gamepad) {
-		for (int i = 1; i < Constants.kNumButtonsPlusOne; ++i) {
-			btns[i] = gamepad.getRawButton(i);
-		}
-	}
-	
+    }
+    
 
-	/** 
+
+    // From CTRE Example code. Credit to CTRE.
+
+    /** 
 	 * Determines if SensorSum or SensorDiff should be used 
 	 * for combining left/right sensors into Robot Distance.  
 	 * 
@@ -307,7 +206,7 @@ public class Robot extends TimedRobot {
 	 * @param _rightInvert2 Invert of the Master Talon
 	 * @param masterConfig Configuration object to fill
 	 */
-	void setRobotTurnConfigs(boolean _rightInvert2, TalonFXConfiguration masterConfig){
+	void setRobotTurnConfigs(boolean _rightInvert2, TalonSRXConfiguration masterConfig){
 		/**
 		 * Determine if we need a Sum or Difference.
 		 * 
