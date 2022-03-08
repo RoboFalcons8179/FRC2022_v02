a901afea5293bf446489eb54f452281bd7f522e8
RoboFalcons8179 <dcrobotics@gmail.com>
2022-03-07 20:01:59 -0500

    ADDED BUTTONS

---
+++ src/main/java/frc/robot/Buttons.java
@@ -0,0 +1,56 @@
+package frc.robot;
+
+public class Buttons {
+    // big list of Button numbers to make code more digestable
+
+    //SLOT 0: xbox_0
+    public int xbox_0_a = 1;
+    public int xbox_0_b = 2;
+    public int xbox_0_x = 3;
+    public int xbox_0_y = 4;
+    public int xbox_0_lbumper = 5;
+    public int xbox_0_rbumper = 6;
+    public int xbox_0_lmenu = 7;
+    public int xbox_0_rmenu = 8;
+    public int xbox_0_l3 = 9;
+    public int xbox_0_r3 = 10;
+
+    //SLOT 1: xbox_1
+    public int xbox_1_a = 1;
+    public int xbox_1_b = 2;
+    public int xbox_1_x = 3;
+    public int xbox_1_y = 4;
+    public int xbox_1_lbumper = 5;
+    public int xbox_1_rbumper = 6;
+    public int xbox_1_lmenu = 7;
+    public int xbox_1_rmenu = 8;
+    public int xbox_1_l3 = 9;
+    public int xbox_1_r3 = 10;
+
+    //SLOT 2: gamepad - numbers heavily up for debate
+    public int gamepad_1 = 1;
+    public int gamepad_2 = 2;
+    public int gamepad_3 = 3;
+    public int gamepad_4 = 4;
+    public int gamepad_5 = 5;
+    public int gamepad_6 = 6;
+    public int gamepad_7 = 7;
+    public int gamepad_8 = 8;
+    public int gamepad_9 = 9;
+    public int gamepad_10 = 10;
+    public int gamepad_11 = 11;
+    public int gamepad_12 = 12;
+    
+    public int gamepad_13 = 13;
+    public int gamepad_14 = 14;
+    public int gamepad_15 = 15;
+    public int gamepad_16 = 16;
+    public int gamepad_17 = 17;
+    public int gamepad_18 = 18;
+    public int gamepad_19 = 19;
+    public int gamepad_20 = 20;
+    public int gamepad_21 = 21;
+    public int gamepad_22 = 22;
+    public int gamepad_23 = 23;
+    public int gamepad_24 = 24;
+}
