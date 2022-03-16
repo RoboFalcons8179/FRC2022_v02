c5dfe6dfc867e3c63406893b25bd2729e21678ac
tciarelli <tciarelli12@gmail.com>
2022-03-15 22:35:48 -0400

    test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,7 +27,7 @@ import edu.wpi.first.wpilibj.Timer;
 
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * The VM is configured to automatically run this class, and to call the functions corresponding too
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
